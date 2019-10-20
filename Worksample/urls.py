c6cc77ef58f371eb9a5a7f1ae9b7beceecad6e0a
Sam Feng <boshenfeng@gmail.com>
2019-10-20 12:33:32 -0700

    add house model

--- Worksample/urls.py
+++ Worksample/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.homepage, name = 'home'),
     path('home.html', views.homepage, name = 'home'),
+    path('admin/', admin.site.urls),
     path('signup.html', views.signup, name='signup')
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
