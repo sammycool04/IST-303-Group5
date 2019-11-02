a60ea07486316d2cccac4428fed5a94c0a736ef7
Chaoyi Wang <chaoyiw1@uci.edu>
2019-11-01 18:51:47 -0700

    static root

--- Worksample/settings.py
+++ Worksample/settings.py
@@ -118,3 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static1")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+if __name__=='__main__':
+    print(os.getcwd())
+    print(BASE_DIR)
