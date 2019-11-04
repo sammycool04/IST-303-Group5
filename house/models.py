7c8e4d13bb1d22b4b2e167421be9453eeefdf8c6
Sam Feng <boshenfeng@gmail.com>
2019-11-03 22:54:45 -0800

    add basic search filter

--- house/models.py
+++ house/models.py
@@ -21,6 +21,6 @@ class House(models.Model):
     # publish_date = models.DateTimeField(auto_now_add = True)
     # views = models.IntegerField(default=0)
     # reviewed = models.BooleanField(default=False)
-    # 
+    #
     # def __str__(self):
     #     return self.summary
--- house/views.py
+++ house/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-
 from .models import House
 
 # Create your views here.
-def house(request):
-    house = House.objects
-    return render(request, 'house/home.html', {'house': house})
+# def house(request):
+#     house = House.objects
+#     return render(request, 'house/home.html', {'house': house})
 
 def BootstrapFilterView(request):
-    return render(request, 'bootstrap_form.html', {})
+    qs = House.objects.all()
+    title_contains_query = request.GET.get('title_contains')
+    # title_exact_query = request.GET.get('title_exact')
+    # title_or_author_query = request.GET.get('title_or_author')
+    # print(title_contains)
+    if title_contains_query != '' and title_contains_query is not None:
+        qs = qs.filter(summary__icontains=title_contains_query)
+    context = {
+        'queryset':qs
+    }
+    return render(request, 'bootstrap_form.html',context)
